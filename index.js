34c00f1e11f62b1b03d5f44b9b12706cdbca9b7b
Raymond Oluoch <rasholuoch@gmail.com>
2020-12-15 00:59:11 +0300

    app working and remaining to add unit tests

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello There! - from Raymond');
+mongoose.connect(
+  'mongodb+srv://shortster:Shortster123@cluster0.qlcga.mongodb.net/shortster?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render('index', { shortUrls });
 });
 
-app.listen(process.env.PUBLIC_PORT, () => {
-  console.log('Server running');
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+
+  res.redirect('/');
 });
+
+// eslint-disable-next-line consistent-return
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  // eslint-disable-next-line no-plusplus
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 5000);
---
+++ models/shortUrl.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const shortId = require('shortid');
+
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+    required: true,
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  visitedAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+module.exports = mongoose.model('ShortUrl', shortUrlSchema);
